9b576ba58fcdc3497caeb234a20cb940a98c3394
Victor Cassen <vcassen@bento.systemsbiology.net>
2011-03-17 13:40:44 -0700

    adding test features, et al

---
+++ t/pipeline/test_create.py
@@ -0,0 +1,27 @@
+import unittest
+from Rnaseq import *
+from warn import *
+
+class TestCreate(unittest.TestCase):
+    def setUp(self):
+        pass
+
+class TestGood(TestCreate):
+    def runTest(self):
+        warn("got here")
+        readset=Readset(name='readset').load()
+        p=Pipeline(name='test', readset=readset)
+        print "p is %s" % p
+
+
+class TestBad(TestCreate):
+        readset=Readset(name='readset').load()
+        try:
+            p=Pipeline(name='test', readset=readset).load()
+        except UserError as ue:
+            
+        print "p is %s" % p
+
+if __name__=='__main__':
+    unittest.main()
+
